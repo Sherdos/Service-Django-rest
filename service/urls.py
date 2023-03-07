66a620e53e7cd8139d9cd12dca8b966a17e7c9da
Sherdos <sherdos26@gmail.com>
2023-03-07 12:26:16 +0600

    class UpdateAPIView, class RetrieveUpdateDestroyAPIView

--- service/urls.py
+++ service/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('api/v1/servicelist/', ServiceAPIList.as_view(), name='service' ),
-    path('api/v1/servicelist/<int:pk>/', ServiceAPIList.as_view(), name='service' ),
+    path('api/v1/servicelist/<int:pk>/', ServiceAPIUpdate.as_view(), name='service' ),
+    path('api/v1/servicedetail/<int:pk>/', ServiceAPIDetailView.as_view(), name='service' ),
 ]
--- service/views.py
+++ service/views.py
@@ -1,6 +1,4 @@
 from rest_framework import generics
-from rest_framework.response import Response
-from .models import*
 from .serializers import*
 
 
@@ -12,29 +10,11 @@ class ServiceAPIList(generics.ListCreateAPIView):
     serializer_class = ServiceSerializer
         
         
-    def put(self, request,*args, **kwargs):
-        pk = kwargs.get('pk', None)
-        if not pk:
-            return Response({"error":"Metod PUT not allowed"})
-        try:
-            instance = Service.objects.get(pk=pk)
-        except:
-            return Response({"error":"Object does not exists"})
-        
-        
-        serializer = ServiceSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'post':serializer.data})
-        
-    def delete(self, request,*args, **kwargs):
-        pk = kwargs.get('pk', None)
-        if not pk:
-            return Response({"error":"Metod DELETE not allowed"})
-        try:
-            instance = Service.objects.get(pk=pk)
-            instance.delete()
-        except:
-            return Response({"error":"Object does not exists"})
-        return Response({'post':'delete post '+str(pk)})
-        
+
+class ServiceAPIUpdate(generics.UpdateAPIView):
+    queryset = Service.objects.all()
+    serializer_class=ServiceSerializer
+
+class ServiceAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
